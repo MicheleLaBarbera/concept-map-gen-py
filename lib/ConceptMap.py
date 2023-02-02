7f79228185d472b3edf4c390bc0440d6ac0676a0
MicheleLaBarbera <michele.labarbera8@outlook.it@>
2023-02-02 17:20:27 +0100

    feat(conceptMap): change concept map generation

--- lib/ConceptMap.py
+++ lib/ConceptMap.py
@@ -16,13 +16,13 @@ class ConceptMap:
     nodes_count = random.randint(node_min, node_max) # Generate a number between the minimum and the maximum of nodes count
 
     # Generate a directed acyclic graph (DAG) with 2/3 probability to have the default generated edges direction reversed
-    self.graph = nx.gn_graph(nodes_count) if random.randint(0, 2) == 2 else nx.gn_graph(nodes_count).reverse()
+    self.graph = nx.random_tree(nodes_count, create_using=nx.DiGraph)
 
     # Convert the generated directed acyclic graph (DAG) to an adjacency matrix
     self.adjacency_matrix = nx.adjacency_matrix(self.graph).todense()
 
     # Entropy calculation
-    self.entropy = self.calculate_entropy()
+    self.calculate_entropy()
     return True
 
   def draw(self):
